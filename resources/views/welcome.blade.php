1c5a4f31fcf7f863bb008ffac8e875ce3d7e3604
Karol <karol.wisniewski2901@gmail.com>
2023-09-19 12:40:19 +0200

    font fixesv3

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -63,7 +63,7 @@
                                 UDT
                             </button>
                             @else
-                            <a href="{{ asset('storage/'.$machine->udt_path) }}" target="_blank" type="button" class="px-4 py-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-r-md hover:bg-gray-100 hover:text-orange-700 focus:z-10 focus:ring-2 focus:ring-orange-700 focus:text-orange-700 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-orange-500 dark:focus:text-white">
+                            <a href="{{ asset('storage/'.$machine->udt_path) }}" target="_blank" class="px-4 py-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-r-md hover:bg-gray-100 hover:text-orange-700 focus:z-10 focus:ring-2 focus:ring-orange-700 focus:text-orange-700 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-orange-500 dark:focus:text-white">
                                 UDT
                             </a>
                             @endif
