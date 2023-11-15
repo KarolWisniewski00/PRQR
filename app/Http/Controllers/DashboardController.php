2803c36488060f3815307ad3f084f182abad9a63
Karol <karol.wisniewski2901@gmail.com>
2023-11-15 16:12:48 +0100

    add machines gr26j grc12

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -11,7 +11,7 @@ class DashboardController extends Controller
 {
     public $dict_machine = [
         'genie' => ['GS-1932', 'GS-2046 E-Drive', 'GS-2632', 'GS-2646', 'GS-3246', 'GS-4047', 'GS-4655 E-Drive',
-        '4069-RTOR', '4390-RTOR', '3369-RTOR','3384-RTOR', '2669-RTOR',
+        '4069-RTOR', '4390-RTOR', '3369-RTOR','3384-RTOR', '2669-RTOR', 'GR26J', 'GRC12',
         'Z-30', 'Z-33','Z-34', 'Z-45', 'Z-51', 'Z-60', 'Z-62', 'Z-80','S-80','S-65','S-65XC', 'S-85', 'S-85XC'],
         'jlg' => ['E600JP', 'E450AJ', '4394RT'],
         'magni' => ['ES1612E',],
@@ -127,6 +127,14 @@ class DashboardController extends Controller
                 case 'S-65XC':
                     $instruction_path = 'instruction/s65xc.pdf';
                     $photo_path = 'photo/s65.jpeg';
+                    break;
+                case 'GR26J':
+                    $instruction_path = 'instruction/26j.pdf';
+                    $photo_path = 'photo/gr26j.jpeg';
+                    break;
+                case 'GRC12':
+                    $instruction_path = 'instruction/grc12.pdf';
+                    $photo_path = 'photo/grc12.jpeg';
                     break;
             }
         } elseif ($request->jlg) {
